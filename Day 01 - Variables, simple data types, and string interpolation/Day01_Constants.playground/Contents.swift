df3f66888d00d08a7b8303e4ff9d7f9f59007d92
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-09 17:36:15 -0500

    Day 1 - Constants

---
+++ Day 01 - Variables, simple data types, and string interpolation/Day01_Constants.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// Constants
+/*
+ Son valores que no cambian durante la ejecución del programa.
+ - Se declaran usando la palabra clave `let`.
+ - Una vez asignado el valor, no puede modificarse.
+ - Se recomienda usar constantes por defecto, y variables solo cuando el valor necesite cambiar.
+ 
+ Beneficios:
+ - Mayor seguridad en el código, evita modificaciones accidentales.
+ - Swift puede optimizar el rendimiento al saber que el valor no cambiará.
+
+ Ejemplo:
+ let country = "Perú"
+ let birthYear = 2001
+
+ // country = "México" // ❌ Error: Cannot assign to value: 'country' is a 'let' constant
+
+IMPORTANTE = Cuando programemos en Swift, siempre hay que usar let a menos a que queramos cambiar el valor de una variable a posterior.
+*/
+let nacionalidad = "Perú"
+let idioma = "Español"
