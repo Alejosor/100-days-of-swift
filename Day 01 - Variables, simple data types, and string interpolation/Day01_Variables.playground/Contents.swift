b400aeff55c3ba7c9ed920c72f3bc0c3026c74c4
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-09 08:27:34 -0500

    Day 1: Variables

---
+++ Day 01 - Variables, simple data types, and string interpolation/Day01_Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// Variables
+
+/*Es un lugar donde podemos almacenar data en memoria. Se le llaman varaibles por sus valores pueden variar o ser cambiados.*/
+
+var greeting = "Hello, playground"
+var name = "Alejandro"
+var lastName = "Soriano"
+greeting = "Hola, Discord"
+name = "Pedro"
+lastName = "Cespedes"
+
+/*Solo es necesario al momento de iniciarlizar la variable colocar el "var"*/
+/*Otra cosa a tener en cuenta es que cuando le colocamos por primera vez un tipo de dato a una variable, esta tomará el tipo de dato del valor y se lo asignará a la variable.*/
