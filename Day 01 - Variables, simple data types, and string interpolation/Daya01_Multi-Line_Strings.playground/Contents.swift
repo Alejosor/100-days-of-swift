d8abd5774cf77317f37b135705469d9ae0eac9b9
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-09 09:28:16 -0500

    Day 1 - Multi-Line Strings

---
+++ Day 01 - Variables, simple data types, and string interpolation/Daya01_Multi-Line_Strings.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Multi-Line Strings
+
+// Multi-Line con \ en la impresión (sin salto de línea)
+var Str1 = """
+Este es un
+String
+multilíneas
+"""
+
+// Multi-Line con \ en la impresión (sin salto de línea)
+var Str2 = """
+Este es un \
+String \
+multilíneas \
+sin las /
+"""
