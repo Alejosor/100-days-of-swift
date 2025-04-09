cfb480d01ddcb9b26b15e701bef31e1ad1521b2e
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-09 18:07:34 -0500

    Day 1 - Type_Annotations

---
+++ Day 01 - Variables, simple data types, and string interpolation/Day01_Type_Annotations.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+// Type Annotations
+/*
+ Permiten especificar manualmente el tipo de dato de una variable o constante.
+ - Sintaxis: var nombre: TipoDeDato = valor
+ - Aumenta la claridad del código y ayuda a evitar errores.
+ - Obligatorio cuando declaras una variable sin valor inicial.
+ 
+ Ejemplo:
+ let name: String = "Alejosor"
+ var age: Int = 23
+ var pi: Double = 3.1416
+ var isSwiftFun: Bool = true
+
+ También se usa cuando defines la variable pero asignas el valor después:
+ var score: Int
+ score = 100
+*/
+let str = "Hello, playground"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+// Ejemplo
+
+let nombre: String = "Alejosor"
+let edad: Int = 24
+let altura: Double = 1.82
+let esSwiftGenial: Bool = true
