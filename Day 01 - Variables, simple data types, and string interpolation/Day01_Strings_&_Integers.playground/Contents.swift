0108ce3bcbac5c0378655a5a4ea9d7ad5e412dd7
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-09 08:39:08 -0500

    Day 1 - Strings & Integers

---
+++ Day 01 - Variables, simple data types, and string interpolation/Day01_Strings_&_Integers.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// Strings & Integers
+
+// Strings - Type Str
+// Son cadenas de texto las cuales deben estar entre ("_")
+var saludos = "Hola, playground"
+var despedidas = "Adiós, playground"
+
+// Integers - Type Int
+// Son todos lo números enteros
+var edad = 38
+edad = 20
+// Se puede trabajar con números grandes separandolos con "_"
+var poblacion = 80_000_000 // 80,000,000
+
+/* Importante:
+ Una vez que se asigna un tipo de dato a una variable
+ no podemos usar otro tipo de dato en la misma*/
