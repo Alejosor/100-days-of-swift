c0630518d484a80e68a46556580cd788cdde107b
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-09 12:25:00 -0500

    Day 1 - Doubles_&_Booleans

---
+++ Day 01 - Variables, simple data types, and string interpolation/Daya01_Doubles_&_Booleans.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+// Doubles
+/*
+ Número de punto flotante de doble precisión (64 bits).
+ - Uso general más preciso y recomendado para cálculos normales.
+ - Permite almacenar aproximadamente 15-16 cifras decimales de precisión.
+ - Swift por defecto usa Double para los números decimales.
+ - Ejemplo de uso: cálculos matemáticos, distancias, medidas precisas, etc.
+ */
+var pi = 3.1416
+var l = 150.45
+
+// Booleans
+/*
+ Tipo de dato lógico que solo puede tener dos valores posibles:
+ - true (verdadero)
+ - false (falso)
+ Se utiliza para controlar el flujo de un programa o representar condiciones lógicas.
+ Ejemplo de uso: verificaciones, condiciones, validaciones.
+ */
+var isSwiftAwesome = true
+var hasFinishedDayOne = false
