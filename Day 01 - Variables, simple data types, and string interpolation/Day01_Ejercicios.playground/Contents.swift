c4de077b0f7c93d555d1b02a9357022f7a0018ca
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-04-10 13:56:16 -0500

    Day 1 - Ejercicios Corregidos

--- Day 01 - Variables, simple data types, and string interpolation/Day01_Ejercicios.playground/Contents.swift
+++ Day 01 - Variables, simple data types, and string interpolation/Day01_Ejercicios.playground/Contents.swift
@@ -12,13 +12,13 @@ var suma_de_dos_numeros: Int = 24 + 24
 
 //4.    Declara una constante para tu lugar de nacimiento y muestra un mensaje que diga: “Nací en [lugar]”.
 let lugar_de_nacimiento: String = "Chiclayo, Perú"
-let mensaje_nacimiento: String = "Yo nací \(lugar_de_nacimiento)"
+let mensaje_nacimiento: String = "Yo nací en \(lugar_de_nacimiento)"
 
 //5.    Declara una variable que indique si tienes licencia de conducir (valor booleano).
 var licencia: Bool = false
 
 //6.    Crea una constante que almacene la temperatura actual en grados Celsius como Double.
-let temperatura_Actual: Double = 25
+let temperatura_actual: Double = 25
 
 //7.    Declara dos variables: una con tu nombre y otra con tu apellido. Luego concatena ambas para formar tu nombre completo.
 var nombre: String = "Alejandro"
@@ -31,7 +31,7 @@ var cantidad_productos: Int = 3
 var total_compra: Double = precio_producto * Double(cantidad_productos)
 
 //9.    Utiliza interpolación de strings para crear un mensaje que diga: “Hola, mi nombre es [nombre] y tengo [edad] años”.
-var mensaaje_interpolacion: String = "Hola mi nombre es \(nombre) y tengo \(edad_actual) años"
+var mensaje_interpolacion: String = "Hola mi nombre es \(nombre) y tengo \(edad_actual) años"
 
 //10.    Declara una variable que indique si hoy es un día soleado (Bool).
 var es_soleado: Bool = true
@@ -41,9 +41,9 @@ let ano_nacimiento: Int = 2000
 let edad_calculada: Int = 2025 - ano_nacimiento
 
 //12.    Crea dos variables de tipo Double y realiza la multiplicación de ambas.
-let varaible_double1: Double = 11.5
-let varaible_double2: Double = 22.5
-let multiplicacion_double: Double = varaible_double1 * varaible_double2
+let variable_double1: Double = 11.5
+let variable_double2: Double = 22.5
+let multiplicacion_double: Double = variable_double1 * variable_double2
 
 //13.    Declara una constante que contenga el nombre de tu película favorita.
 let pelicula_favorita: String = "El Padrino"
@@ -54,12 +54,12 @@ var pasos_totales: Int = pasos_hoy + 1000
 
 //15.    Crea una variable para tu altura en metros (Double) y escribe un mensaje que diga: “Mido [altura] metros”.
 var altura_metros: Double = 1.82
-var mesaje: String = "Mido \(altura_metros) metros."
+var mensaje: String = "Mido \(altura_metros) metros."
 
 //16.    Declara dos constantes: una para el precio sin impuesto y otra para el impuesto aplicado. Calcula el precio final.
 let precio_sin_impuesto: Double = 100.5
 let impuesto: Double = 0.15
-let precio_final: String = "El precio final es S/\(precio_sin_impuesto * impuesto) "
+let precio_final: String = "El precio final es S/\(precio_sin_impuesto+(precio_sin_impuesto * impuesto)) "
 
 //17.    Declara una variable para almacenar si estás aprendiendo Swift (Bool) y escribe un mensaje que diga: “¿Estás aprendiendo Swift? [respuesta]”.
 var estudiando_swift: Bool = true
